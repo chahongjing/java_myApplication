43eec8fd7b3554258a4939cf7377d3c29b999889
chahongjing <310510906@qq.com>
2018-01-10 21:59:23 +0800

    代码签入

--- downloadShiTi/src/main/java/com/zjy/App.java
+++ downloadShiTi/src/main/java/com/zjy/App.java
@@ -1,6 +1,7 @@
 package com.zjy;
 
 import com.zjy.service.Ibm_PaperQuestionService;
+import com.zjy.service.TestBeanService;
 import com.zjy.utils.LogHelper;
 import org.slf4j.Logger;
 
@@ -8,18 +9,17 @@ import org.slf4j.Logger;
  * Hello world!
  */
 public class App {
+    protected static Logger logger = LogHelper.getLogger(App.class);
+
     public static void main(String[] args) {
-        Logger logger = LogHelper.getLogger(App.class);
-        try {
-            logger.info("开始导出数据！");
-            Ibm_PaperQuestionService ibm_paperQuestionService = new Ibm_PaperQuestionService();
-            ibm_paperQuestionService.exportAllXueKePaperQuestionExcel();
-            String message = "数据全部导出完成！！！请在“" + ibm_paperQuestionService.getExcelPath() + "”查看导出的内容！";
-            logger.info(message);
-            System.in.read();
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.error("生成excel异常！", e);
-        }
+//        try {
+//            new Ibm_PaperQuestionService().exportAllXueKePaperQuestionExcel();
+//            System.in.read();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            logger.error("生成excel异常！", e);
+//        }
+        TestBeanService beanService = new TestBeanService();
+        beanService.getBeans();
     }
 }
